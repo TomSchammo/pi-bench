6ea237ddeef80f4bdd375efb910054275065e8cb
Tom Schammo <tom@tomschammo.com>
2025-10-26 12:40:26 +0100

    feat: Add utility macros to make benchmarking more convenient
    - Automatically allocates and sets GT
    - Automatically determines number of benchmarks for benchmarking and
      evaluation
    - Macros to run and evaluate benchmarks as well as to print results

--- include/utils.h
+++ include/utils.h
@@ -2,6 +2,7 @@
 #define UTILS_H
 
 #include "./bench.h"
+#include "./data_processing.h"
 #include <stdint.h>
 #include <stdlib.h>
 
@@ -44,13 +45,161 @@ static inline benchmark_t *setup_benchmark(const char *name,
 
 static inline void cleanup_benchmark(benchmark_t *benchmark,
                                      bool free_output_buffer) {
+  bool gt_freed = false;
+
   if (free_output_buffer) {
     free(benchmark->results->output_buffer);
   }
+
+  if (benchmark->is_baseline && !gt_freed) {
+    free(benchmark->results->gt);
+  }
+
   free(benchmark->results->samples);
   free(benchmark->results->cache_miss_rates);
   free(benchmark->results);
   free(benchmark);
 }
+
+#define BENCHMARK_TIME_PINNED(name, is_baseline, validate, output_buffer,      \
+                              size, core, func)                                \
+  +1
+#define BENCHMARK_TIME(name, is_baseline, validate, output_buffer, size, func) \
+  +1
+#define BENCHMARK_CYCLES_PINNED(name, is_baseline, validate, output_buffer,    \
+                                size, core, func)                              \
+  +1
+#define BENCHMARK_CYCLES(name, is_baseline, validate, output_buffer, size,     \
+                         func)                                                 \
+  +1
+enum { BENCHMARK_COUNT = (0 BENCHMARKS) };
+#undef BENCHMARK_TIME_PINNED
+#undef BENCHMARK_TIME
+#undef BENCHMARK_CYCLES_PINNED
+#undef BENCHMARK_CYCLES
+
+static benchmark_t *_benchmark_array[BENCHMARK_COUNT];
+static size_t _benchmark_idx = 0;
+
+#define BENCHMARK_TIME_PINNED(name, is_baseline, validate, output_buffer,      \
+                              size, core, func)                                \
+  {                                                                            \
+                                                                               \
+    printf("\n=== %s Benchmark ===\n", name);                                  \
+                                                                               \
+    benchmark_t *benchmark =                                                   \
+        setup_benchmark(name, WARMUP_RUNS, TIMED_RUNS, is_baseline, validate,  \
+                        output_buffer, size);                                  \
+                                                                               \
+    if (is_baseline) {                                                         \
+      gt = benchmark->results->gt;                                             \
+    } else {                                                                   \
+      benchmark->results->gt = gt;                                             \
+    }                                                                          \
+                                                                               \
+    BENCHMARK_FUNC_PINNED(func, benchmark, core);                              \
+                                                                               \
+    memset(output_buffer, 0, size);                                            \
+    _benchmark_array[_benchmark_idx++] = benchmark;                            \
+  }
+
+#define BENCHMARK_TIME(name, is_baseline, validate, output_buffer, size, func) \
+  {                                                                            \
+                                                                               \
+    printf("\n=== %s Benchmark ===\n", name);                                  \
+                                                                               \
+    benchmark_t *benchmark =                                                   \
+        setup_benchmark(name, WARMUP_RUNS, TIMED_RUNS, is_baseline, validate,  \
+                        output_buffer, size);                                  \
+                                                                               \
+    if (is_baseline) {                                                         \
+      gt = benchmark->results->gt;                                             \
+    } else {                                                                   \
+      benchmark->results->gt = gt;                                             \
+    }                                                                          \
+                                                                               \
+    BENCHMARK_FUNC(func, benchmark);                                           \
+                                                                               \
+    memset(output_buffer, 0, size);                                            \
+    _benchmark_array[_benchmark_idx++] = benchmark;                            \
+  }
+
+#define BENCHMARK_CYCLES_PINNED(name, is_baseline, validate, output_buffer,    \
+                                size, core, func)                              \
+  {                                                                            \
+                                                                               \
+    printf("\n=== %s Benchmark ===\n", name);                                  \
+                                                                               \
+    benchmark_t *benchmark =                                                   \
+        setup_benchmark(name, WARMUP_RUNS, TIMED_RUNS, is_baseline, validate,  \
+                        output_buffer, size);                                  \
+                                                                               \
+    if (is_baseline) {                                                         \
+      gt = benchmark->results->gt;                                             \
+    } else {                                                                   \
+      benchmark->results->gt = gt;                                             \
+    }                                                                          \
+                                                                               \
+    BENCHMARK_FUNC_CYCLES_PINNED(func, benchmark, core);                       \
+                                                                               \
+    memset(output_buffer, 0, size);                                            \
+    _benchmark_array[_benchmark_idx++] = benchmark;                            \
+  }
+
+#define BENCHMARK_CYCLES(name, is_baseline, validate, output_buffer, size,     \
+                         func)                                                 \
+  {                                                                            \
+                                                                               \
+    printf("\n=== %s Benchmark ===\n", name);                                  \
+                                                                               \
+    benchmark_t *benchmark =                                                   \
+        setup_benchmark(name, WARMUP_RUNS, TIMED_RUNS, is_baseline, validate,  \
+                        output_buffer, size);                                  \
+                                                                               \
+    if (is_baseline) {                                                         \
+      gt = benchmark->results->gt;                                             \
+    } else {                                                                   \
+      benchmark->results->gt = gt;                                             \
+    }                                                                          \
+                                                                               \
+    BENCHMARK_FUNC_CYCLES(func, benchmark);                                    \
+                                                                               \
+    memset(output_buffer, 0, size);                                            \
+    _benchmark_array[_benchmark_idx++] = benchmark;                            \
+  }
+
+#define RUN_BENCHMARKS() BENCHMARKS
+
+#define PRINT_RESULTS_INDIVIDUAL()                                             \
+  do {                                                                         \
+    printf("\n=== Individual Benchmark Results ===\n");                        \
+    if (BENCHMARK_COUNT > 0) {                                                 \
+      for (size_t i = 0; i < BENCHMARK_COUNT; i++) {                           \
+        benchmark_t *b = _benchmark_array[i];                                  \
+        calculate_stats(b->results, b->timed_iterations);                      \
+        print_result(b);                                                       \
+      }                                                                        \
+    }                                                                          \
+  } while (0)
+
+#define PRINT_RESULTS_GROUP(print_invalid)                                     \
+  do {                                                                         \
+    printf("=== Comparative Results ===\n");                                   \
+    print_results(_benchmark_array, BENCHMARK_COUNT, print_invalid);           \
+  } while (0)
+
+#define SAVE(dir)                                                              \
+  do {                                                                         \
+    to_csv(_benchmark_array, BENCHMARK_COUNT, dir);                            \
+  } while (0)
+
+#define CLEANUP()                                                              \
+  do {                                                                         \
+    if (BENCHMARK_COUNT > 0) {                                                 \
+      for (size_t i = 0; i < BENCHMARK_COUNT; i++) {                           \
+        cleanup_benchmark(_benchmark_array[i], false);                         \
+      }                                                                        \
+    }                                                                          \
+  } while (0)
 
 #endif // UTILS_H
